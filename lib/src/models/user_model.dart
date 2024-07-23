24709e4766bc1739cf37cf3f971b5381fadd7538
Fazo201 <erkaboyevfazogir98@gmail.com>
2024-07-23 16:30:19 +0500

    fazogir __________________

--- lib/src/models/user_model.dart
+++ lib/src/models/user_model.dart
@@ -4,6 +4,7 @@ UserModel userModelFromJson(String str) => UserModel.fromJson(json.decode(str));
 
 String userModelToJson(UserModel data) => json.encode(data.toJson());
 
+
 class UserModel {
   UserModel({
     this.id,
